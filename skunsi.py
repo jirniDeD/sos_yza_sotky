cb30a692a922c3187a05f9380bfb3fe5a451c3bf
203 student <kostaakovlev111@gmail.com>
2025-01-13 11:22:02 +0200

    ya chmo

---
+++ skunsi.py
@@ -0,0 +1,26 @@
+from Tools.scripts.generate_sre_constants import sre_constants_header
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hell(self):
+        print(f"Hello!My name is {self.name}")
+
+
+    def get_info(self):
+        return f"{self.name};{self.age}"
+
+
+vasya = Student("vasya", 15)
+petya = Student("petya", 17)
+anna = Student("anna", 111)
+print(vasya)
+print(vasya.age, vasya.name)
+print(petya.age, petya.name)
+print(anna.age, anna.name)
+vasya.say_hell()
+print(vasya.get_info())
+del vasya
