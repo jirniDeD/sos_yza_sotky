094c3d69b10370029a677773c18da28032cbf710
203 student <kostaakovlev111@gmail.com>
2025-01-13 11:42:37 +0200

    ya chmo2

--- skunsi.py
+++ skunsi.py
@@ -1,27 +1,48 @@
 from Tools.scripts.generate_sre_constants import sre_constants_header
 
 
-class Student:
-    def __init__(self, name, age):
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def say_hell(self):
+#         print(f"Hello!My name is {self.name}")
+#
+#
+#     def get_info(self):
+#         return f"{self.name};{self.age}"
+#
+#
+# vasya = Student("vasya", 15)
+# petya = Student("petya", 17)
+# anna = Student("anna", 111)
+# print(vasya)
+# print(vasya.age, vasya.name)
+# print(petya.age, petya.name)
+# print(anna.age, anna.name)
+# vasya.say_hell()
+# print(vasya.get_info())
+#
+# del vasya
+
+
+
+
+class Animal:
+    animal_counter = 0
+    def __init__(self, animal_type, name):
+        self.animal_type = animal_type
         self.name = name
-        self.age = age
+        Animal.animal_counter += 1
 
-    def say_hell(self):
-        print(f"Hello!My name is {self.name}")
-
-
-    def get_info(self):
-        return f"{self.name};{self.age}"
+    def show_info(self):
+        print(f"{self.animal_type};{self.name}")
 
 
-vasya = Student("vasya", 15)
-petya = Student("petya", 17)
-anna = Student("anna", 111)
-print(vasya)
-print(vasya.age, vasya.name)
-print(petya.age, petya.name)
-print(anna.age, anna.name)
-vasya.say_hell()
-print(vasya.get_info())
-
-del vasya
+barsik = Animal("Cat", "Barsik")
+bobik = Animal("dog", "Bobik")
+barsik.show_info()
+print(Animal.animal_counter)
+print(bobik.animal_counter)
+print(barsik.animal_counter)
