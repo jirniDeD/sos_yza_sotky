5daf019b1537921e02ad59da37274d9027432e35
203 Student <gnezdilov_artem00@mail.ru>
2025-01-17 15:22:12 +0200

    new

--- skunsi.py
+++ skunsi.py
@@ -1,5 +1,6 @@
 from Tools.scripts.generate_sre_constants import sre_constants_header
 
+import math
 
 # class Student:
 #     def __init__(self, name, age):
@@ -29,20 +30,161 @@ from Tools.scripts.generate_sre_constants import sre_constants_header
 
 
 
-class Animal:
-    animal_counter = 0
-    def __init__(self, animal_type, name):
-        self.animal_type = animal_type
-        self.name = name
-        Animal.animal_counter += 1
+# class Animal:
+#     animal_counter = 0
+#     def __init__(self, animal_type, name):
+#         self.animal_type = animal_type
+#         self.name = name
+#         Animal.animal_counter += 1
+#
+#     def show_info(self):
+#         print(f"{self.animal_type};{self.name}")
+#
+#
+# barsik = Animal("Cat", "Barsik")
+# bobik = Animal("dog", "Bobik")
+# barsik.show_info()
+# print(Animal.animal_counter)
+# print(bobik.animal_counter)
+# print(barsik.animal_counter)
 
-    def show_info(self):
-        print(f"{self.animal_type};{self.name}")
 
 
-barsik = Animal("Cat", "Barsik")
-bobik = Animal("dog", "Bobik")
-barsik.show_info()
-print(Animal.animal_counter)
-print(bobik.animal_counter)
-print(barsik.animal_counter)
+# class Weather:
+#     @staticmethod
+#     def celsium_to_farenheit(t):
+#         return t * 9/5 + 32
+#
+#     @staticmethod
+#     def celsium_to_kelvin(t):
+#         return t + 273.15
+#
+#     def farenheit_to_celsium(t):
+#         return (t - 32) * 5/9
+#
+#     @staticmethod
+#     def farenheit_to_kelvins(t):
+#         return (t - 32) * 5/9 + 273.15
+#
+#     @staticmethod
+#     def kelvin_to_celsium(t):
+#         return t - 273.15
+#
+#     @staticmethod
+#     def kelvin_to_farenheit(t):
+#         return (t - 273.15) * 9/5 + 32
+#
+#
+#
+# print(Weather.celsium_to_farenheit(2))
+# print(Weather.celsium_to_kelvin(0))
+# print(Weather.farenheit_to_celsium(0))
+# print(Weather.farenheit_to_kelvins(0))
+# print(Weather.kelvin_to_celsium(0))
+# print(Weather.kelvin_to_farenheit(0))
+
+
+
+
+class Fraction:
+    def __init__(self, numerator,denumenator):
+        self.a = numerator
+        self.b = denumenator
+        self.reduction()
+
+    def __str__(self):
+        return f"{self.a}/{self.b}"
+
+    def multiplication(self, other):
+        return Fraction(self.a * other.a, self.b * other.b)
+
+    def division(self,other):
+        return Fraction(self.a * other.b, self.b * other.a)
+
+    def __add__(self, other):
+        return Fraction(self.a * other.b + self.b * other.a, self.b * other.b)
+
+    def __sub__(self, other):
+        return Fraction(self.a * other.b - self.b * other.a, self.a * other.b)
+
+    def __mul__(self, other):
+        return Fraction(self.a * other.a, self.b * other.b)
+
+    def __truediv__(self, other):
+        return Fraction(self.a * other.b, self.b * other.a)
+
+    def __iadd__(self, other):
+        self.a = self.a * other.b + self.b*other.a
+        self.b = self.b * other.b
+        return self
+
+    def __isub__(self, other):
+        self.a = self.a * other.b - self.b*other.a
+        self.b = self.b * other.b
+        return self
+
+    def __imul__(self, other):
+        self.a = self.a * other.a
+        self.b = self.b * other.b
+        return self
+
+    def __itruediv__(self, other):
+        self.a, self.b = self.a * other.b, self.b * other.a
+        return self
+    def __gt__(self,other):
+        return self.a * other.b > self.b * other.a
+
+    def __lt__(self, other):
+        return self.a * other.b < self.b * other.a
+
+    def __eq__(self, other):
+        return self.a * other.b == self.b * other.a
+
+    def __ne__(self, other):
+        return self.a * other.b != self.b * other.a
+
+    def __ge__(self, other):
+        return self.a * other.b >= self.b * other.a
+
+    def __le__(self, other):
+        return self.a * other.b <= self.b * other.a
+
+    def reduction(self):
+        common_divider = math.gcd(self.a, self.b)
+        self.a = self.a // common_divider
+        self.b = self.b // common_divider
+
+
+
+
+first = Fraction(2, 5)
+print(first)
+second = Fraction(2, 3)
+print(first.multiplication(second))
+print(first.division(second))
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+first += second
+print(first)
+first -= second
+print(first)
+print(first > second)
+
+
+print(Fraction(50, 100) * (Fraction(2, 2)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
